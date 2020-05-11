1e37fe975e74a7ca524286c440a63ca92b883aa9
Pablo Ríos <56959508+rivers054@users.noreply.github.com>
2020-05-11 18:49:09 +0200

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World!";
+    private final String message = "Prueba del TFG de Pablo Ríos";
 
     public App() {}
 
